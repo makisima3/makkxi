1da5e23c6770cf707e16d9b0e55d92f4a13178f7
makisima3 <maksimya@bk.ru>
2019-11-25 20:56:17 +0300

    add

---
+++ CustomGenericStrucrures/CustomGenericStrucrures/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CustomGenericStructures;
+using listfunc;
+
+namespace Programm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число элементов списка");
+            int count = Convert.ToInt32(Console.ReadLine());
+            CustomGenericStructures.List<double> L = new CustomGenericStructures.List<double>();
+            listfunctions.RandomFill(L, count);
+            Console.WriteLine("Вывод элементов списка(нажмите enter для продолжения)");
+            listfunctions.ConsoleWriteList(L);
+            Console.ReadKey();
+
+            Console.WriteLine("Введите число элементов связного списка");
+            count = Convert.ToInt32(Console.ReadLine());
+            CustomGenericStructures.LinkedList<double> LList = new CustomGenericStructures.LinkedList<double>();
+            listfunctions.RandomFill(LList, count);
+            Console.WriteLine("Вывод элементов связного списка(нажмите enter для продолжения)");
+            listfunctions.ConsoleWriteList(LList);
+            Console.ReadKey();
+
+            Console.WriteLine("Введите число элементов двусвязного списка");
+            count = Convert.ToInt32(Console.ReadLine());
+            DoublyLinkedList<double> DLList = new DoublyLinkedList<double>();
+            listfunctions.RandomFill(DLList, count);
+            Console.WriteLine("Вывод элементов двусвязного списка");
+            listfunctions.ConsoleWriteList(DLList);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CustomGenericStrucrures/CustomGenericStrucrures/listfunctions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CustomGenericStructures;
+
+namespace listfunc
+{
+    public class listfunctions
+    {
+        public static Random rnd = new Random();
+        public static void RandomFill(IList<double> list, int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                list.Add(Math.Round(rnd.NextDouble(), 2));
+            }
+        }
+        public static void ConsoleWriteList(IList<double> list)
+        {
+            foreach (var c in list)
+            {
+                Console.WriteLine(c.ToString());
+            }
+        }
+    }
+}
